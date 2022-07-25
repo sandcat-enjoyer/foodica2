8a0acbc8d421244bf37be0d5c9edbb090114c24d
julings <jules.debbaut14@gmail.com>
2022-07-25 17:14:11 +0200

    Further WidgetKit implementation + additional fixes in manual add page

--- ios/CalorieWidget/CalorieWidget.swift
+++ ios/CalorieWidget/CalorieWidget.swift
@@ -11,40 +11,72 @@ import Intents
 
 struct Provider: IntentTimelineProvider {
     func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date(), configuration: ConfigurationIntent())
+        SimpleEntry(date: Date(), flutterData: nil)
     }
 
     func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date(), configuration: configuration)
+        let entry = SimpleEntry(date: Date(), flutterData: nil)
         completion(entry)
     }
 
     func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
         var entries: [SimpleEntry] = []
+        
+        let sharedDefaults = UserDefaults.init(suiteName: "group.julesdebbaut.foodica")
+        var flutterData: FlutterData? = nil
+        
+        if (sharedDefaults != nil) {
+            do {
+                let shared = sharedDefaults?.string(forKey: "widgetData")
+                if (shared != nil) {
+                    let decoder = JSONDecoder()
+                    flutterData = try decoder.decode(FlutterData.self, from: shared!.data(using: .utf8)!)
+                }
+            }
+            catch {
+                print(error)
+            }
+        }
 
         // Generate a timeline consisting of five entries an hour apart, starting from the current date.
         let currentDate = Date()
         for hourOffset in 0 ..< 5 {
             let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate, configuration: configuration)
+            let entry = SimpleEntry(date: entryDate,flutterData: flutterData)
             entries.append(entry)
         }
-
         let timeline = Timeline(entries: entries, policy: .atEnd)
         completion(timeline)
     }
 }
 
+struct FlutterData: Decodable, Hashable {
+    let text: String
+}
+
 struct SimpleEntry: TimelineEntry {
     let date: Date
-    let configuration: ConfigurationIntent
+    let flutterData: FlutterData?
 }
 
 struct CalorieWidgetEntryView : View {
     var entry: Provider.Entry
+    
+    private var FlutterDataView: some View {
+        Text(entry.flutterData!.text)
+    }
+    
+    private var NoDataView: some View {
+        Text("No data found.")
+    }
 
     var body: some View {
-        Text(entry.date, style: .time)
+        if (entry.flutterData == nil) {
+            NoDataView
+        }
+        else {
+            FlutterDataView
+        }
     }
 }
 
@@ -63,7 +95,7 @@ struct CalorieWidget: Widget {
 
 struct CalorieWidget_Previews: PreviewProvider {
     static var previews: some View {
-        CalorieWidgetEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent()))
+        CalorieWidgetEntryView(entry: SimpleEntry(date: Date(), flutterData: nil))
             .previewContext(WidgetPreviewContext(family: .systemSmall))
     }
 }
